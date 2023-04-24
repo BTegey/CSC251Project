7af9a5ac09489cfcf462c6a6255c8f6afd1dc2cf
BTegey <btegey00@email.cpcc.edu>
2023-04-24 14:46:58 -0400

    Create PolicyHolder.java

---
+++ PolicyHolder.java
@@ -0,0 +1,10 @@
+/*
+   Brandon Tegey
+   04/24/2023
+   This class holds all the information provided by the user input into the program about a 
+   policy holder of an insurance policy.
+*/
+public class PolicyHolder {
+   
+   
+}
